5bdc8651365313d476c6f02ce874683b17a7bcb6
Belinda Slakman <slakman.b@husky.neu.edu>
2016-05-25 16:27:53 -0400

    Have checkEa script count the number of unsuccessful reactions or negative Ea.

--- checkEa.py
+++ checkEa.py
@@ -1,17 +1,22 @@
 import os
 
-family = ['H_Abstraction']
+family = 'H_Abstraction'
 directory = os.path.join("/home/slakman.b/Gaussian/SMD/", family)
 
 Ea_checkfile = open(os.path.join(directory, "Ea_check.txt"), 'w')
 
+total = 0
+unsuccessful = 0
+negative = 0
+
 # Go through all of the reactions for that family
 for rxn_folder in os.listdir(directory):
+    total += 1
     rxn_directory = os.path.join(directory, rxn_folder)
 
-    if not os.path.exists(os.path.join(rxn_directory, output.txt)):
-        Ea_checkfile.write("No output file for {0}".format(str(rxn_folder)))
-
+    if not os.path.exists(os.path.join(rxn_directory, "output.txt")):
+        Ea_checkfile.write("No output file for {0}\n".format(str(rxn_folder)))
+        unsuccessful += 1
     else:
         with open(os.path.join(rxn_directory, "output.txt"), 'r') as output_file:
             lines = output_file.read().split('\n')
@@ -19,6 +24,7 @@ for rxn_folder in os.listdir(directory):
                 if "Activation energy" in l:
                     Ea = float(l.split()[-1])
                     if Ea < 0:
-                        Ea_checkfile.write("Ea of {0} for {1}".format(Ea, rxn_folder))
-
+                        negative += 1
+                        Ea_checkfile.write("Ea of {0} for {1}\n".format(Ea, rxn_folder))
+Ea_checkfile.write("total: {0}, unsuccessful = {1}, negative = {2}".format(total, unsuccessful, negative))
 Ea_checkfile.close()
