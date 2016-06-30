9666ea3a5bce0b776fdc15a21cafe911618e26b5
Belinda Slakman <slakman.b@husky.neu.edu>
2016-06-30 11:46:12 -0400

    SMD script now creates a pandas data frame with all training data.
    The data frame includes attributes up to the nearest neighbor count
    and the calculated barrier correction.

--- SMD.py
+++ SMD.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-# A script for gathering the SMD barrier correction values from Gaussian jobs.
+# A script for gathering the SMD barrier correction values from Gaussian jobs and putting into the RMG database.
+# Also, creates a .csv file that can be used for later processing, like making a decision tree regressor.
 
 import os.path
 import argparse
@@ -10,6 +11,7 @@ from rmgpy.species import Species
 from rmgpy.reaction import Reaction
 from rmgpy.data.rmg import RMGDatabase
 from rmgpy import settings
+import pandas as pd
 
 parser = argparse.ArgumentParser(description="""
 Given solvent, will get the delEa values from the output files.
@@ -50,6 +52,22 @@ out.close()
 index = 1
 unique = {}
 
+data = [["X_rad_count",
+"X_lp_count",
+"X_atomType",
+"X_neighborC",
+"X_neighborO",
+"X_neighborH",
+"X_neighborN",
+"Y_rad_count",
+"Y_lp_count",
+"Y_atomType",
+"Y_neighborC",
+"Y_neighborO",
+"Y_neighborH",
+"Y_neighborN",
+"barrier_correction"]]
+
 for subdir, dirs, files in os.walk(data_dir):
     reactionString = os.path.basename(os.path.normpath(subdir))
     #if reactionString in ('H_Abs','solv2'): continue
@@ -88,7 +106,7 @@ for subdir, dirs, files in os.walk(data_dir):
     		tempList = rmgDatabase.kinetics.generateReactionsFromFamilies([moleculeA], [], only_families=[args.family])
     		for rxn0 in tempList:
     			reactionList.append(rxn0)
-    elif args.family in ['R_Addition_MultipleBond']:
+    elif args.family in ['R_Addition_MultipleBond', 'Silylene_Insertion']:
         rSpecies1, rSpecies2 = [Species(molecule=Molecule().fromSMILES(r)) for r in reactants]
     	pSpecies = [Species(molecule=Molecule().fromSMILES(p)) for p in products]
     	rSpecies1.generateResonanceIsomers()
@@ -149,6 +167,8 @@ for subdir, dirs, files in os.walk(data_dir):
     for line in lines:
        if line.startswith("Difference"):
           text, barrierCorrection = line.split("= ")
+          if abs(float(barrierCorrection)) > 10.0:
+              print reactionString
 
     out = open(output_file, 'a')
     out.write('entry( \n')
@@ -183,3 +203,46 @@ for subdir, dirs, files in os.walk(data_dir):
             d.write(prod + '\n')
             d.write(my_reaction.products[indP].toAdjacencyList() + '\n')
             d.close()
+
+    for m in my_reaction.reactants:
+        try:
+            x_atom = m.getLabeledAtom('*1')
+            x_neighbors = [atom for atom in m.getLabeledAtom('*1').bonds]
+        except:
+            pass
+        try:
+            y_atom = m.getLabeledAtom('*3')
+            y_neighbors = [atom for atom in m.getLabeledAtom('*3').bonds]
+        except:
+            pass
+    x_rad = x_atom.radicalElectrons
+    x_lp = x_atom.lonePairs
+    x_atomType = x_atom.atomType.label
+    x_neighborC = 0
+    x_neighborO = 0
+    x_neighborH = 0
+    x_neighborN = 0
+    for neighbor in x_neighbors:
+        if neighbor.element.symbol is 'C': x_neighborC += 1
+        elif neighbor.element.symbol is 'H': x_neighborH += 1
+        elif neighbor.element.symbol is 'O': x_neighborO += 1
+        elif neighbor.element.symbol is 'N': x_neighborN += 1
+        else: print "Encountered {0}; need a new attribute!".format(neighbor.element.symbol)
+
+    y_rad = y_atom.radicalElectrons
+    y_lp = y_atom.lonePairs
+    y_atomType = y_atom.atomType.label
+    y_neighborC = 0
+    y_neighborO = 0
+    y_neighborH = 0
+    y_neighborN = 0
+    for neighbor in y_neighbors:
+        if neighbor.element.symbol is 'C': y_neighborC += 1
+        elif neighbor.element.symbol is 'H': y_neighborH += 1
+        elif neighbor.element.symbol is 'O': y_neighborO += 1
+        elif neighbor.element.symbol is 'N': y_neighborN += 1
+        else: print "Encountered {0}; need a new attribute!".format(neighbor.element.symbol)
+
+    data.append([x_rad, x_lp, x_atomType, x_neighborC, x_neighborO, x_neighborH, x_neighborN, y_rad, y_lp, y_atomType, y_neighborC, y_neighborO, y_neighborH, y_neighborN, barrierCorrection])
+
+pd.DataFrame(data[1:], columns=data[0]).to_csv('data_neighbors.csv')
