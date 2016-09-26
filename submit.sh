f753d3e6d13120f448663728b851e0a6c85d5670
Belinda Slakman <slakman.b@husky.neu.edu>
2016-09-26 11:09:01 -0400

    Fix submit script for AutoTST for SLURM.

--- submit.sh
+++ submit.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#BSUB -J IHsolv[1-67]%10
-#BSUB -o output.log
-#BSUB -R "span[hosts=1]"
-#BSUB -n 10
-#BSUB -q west
-#BSUB -e error_log
+#SBATCH --job-name=si-all
+#SBATCH --array=1-90%5
+#SBATCH -N 1
+#SBATCH -n 20
+#SBATCH --partition=west
+#SBATCH --error=error_log
 
-export GAUSS_SCRDIR="/scratch/slakman.b"
-python solvation_energies.py
+export GAUSS_SCRDIR="/gss_gpfs_scratch/slakman.b"
+python input.py
